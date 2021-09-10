8a61280eb888772203385bd07d811169ab285add
Guilherme <guidumasperes@gmail.com>
2021-09-10 17:05:59 -0300

    updating infos

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function Education() {
       <div className="content-wrap item-details">
         <h2>Educação</h2>
         <section>
-          <h3>Instituto de Computação - IC - Universidade Estadual de Campinas (UNICAMP)</h3>
-          <p>Bacharelado em Ciência da Computação, 2016-2021</p>
+          <h3>Instituto de Computação - IC - Universidade Estadual de Campinas (UNICAMP).</h3>
+          <p>Bacharelado em Ciência da Computação, 2016-2021.</p>
         </section>
       </div>
     </section>
@@ -38,10 +38,13 @@ function WorkExperience() {
             <p>Fevereiro 2021 - Agosto 2021</p>
           </div>
           <div className="job-skill-summary">
-            <p>Desenvolveu uma parte do software de controle interno da plataforma de investimentos da empresa em C# utilizando o Windows Forms e SQL. 
-               Desenvolveu uma versão menor da plataforma de investimentos da empresa usando o Microsoft PowerApps. 
-               Automatizou planilhas do Excel usando a biblioteca openpyxl do Python agilizando as tarefas dos outros funcionários. Prestou suporte ao usuário na utilização do ERP(brERP) da empresa garantindo maior fluidez nos trabalhos.
-               Auxiliou na manutenção da infraestrutura assegurando a continuidade dos serviços.
+            <p>Automatizou planilhas do Excel com a biblioteca openpyxl do Python para agilizar o
+               trabalho do setor financeiro da empresa, programou uma parte do software da plataforma 
+               de investimentos da empresa em C# utilizando o Windows Forms da plataforma .NET e o 
+               Microsoft SQL Server como banco de dados e programou uma versão mais simples dessa mesma
+               plataforma no Microsoft PowerApps. Prestou suporte ao usuário na utilização do 
+               ERP(brERP) e auxiliou na manutenção da infraestrutura garantindo fluidez e 
+               continuidade dos trabalhos da empresa.
             </p>
           </div>
         </section>
@@ -60,8 +63,8 @@ function Skills() {
             <h3>Idiomas</h3>
           </div>
           <div className="job-skill-summary">
-            <p>Inglês intermediário(Cambridge B1 Preliminary)</p>
-            <p>Francês iniciante(nível A1/A2 do CEFR)</p>
+            <p>Inglês intermediário(Cambridge B1 Preliminary).</p>
+            <p>Francês iniciante(nível A1/A2 do CEFR).</p>
           </div>
         </section>
         <section className="job-skill-item">
@@ -69,13 +72,20 @@ function Skills() {
             <h3>Linguagens de programação</h3>
           </div>
           <div className="job-skill-summary">
-            <p>Python - Avançado</p>
-            <p>HTML - Intermediário</p>
-            <p>CSS - Intermediário</p>
-            <p>JavaScript - Intermediário</p>
-            <p>C - Iniciante</p>
-            <p>C# - Iniciante</p>
+            <p>Python - Avançado.</p>
+            <p>JavaScript - Intermediário.</p>
+            <p>C - Iniciante.</p>
+            <p>C# - Iniciante.</p>
             <p>Java - Iniciante.</p>
+          </div>
+        </section>
+        <section className="job-skill-item">
+          <div className="job-skill-details">
+            <h3>Linguagens de marcação e estilo</h3>
+          </div>
+          <div className="job-skill-summary">
+            <p>HTML - Intermediário.</p>
+            <p>CSS - Intermediário.</p>
           </div>
         </section>
         <section className="job-skill-item">
@@ -83,11 +93,11 @@ function Skills() {
             <h3>Ferramentas e tecnologias</h3>
           </div>
           <div className="job-skill-summary">
-            <p>GitHub - Intermediário</p>
-            <p>Django - Iniciante</p>
-            <p>React.js - Iniciante</p>
-            <p>Node.js - Iniciante</p>
-            <p>REST APIs - Iniciante</p>
+            <p>GitHub - Intermediário.</p>
+            <p>Django - Iniciante.</p>
+            <p>React.js - Iniciante.</p>
+            <p>Node.js - Iniciante.</p>
+            <p>REST APIs - Iniciante.</p>
           </div>
         </section>
         <section className="job-skill-item">
@@ -95,7 +105,7 @@ function Skills() {
             <h3>Processos</h3>
           </div>
           <div className="job-skill-summary">
-            <p>Scrum - Iniciante</p>
+            <p>Scrum - Iniciante.</p>
           </div>
         </section>
         <section className="job-skill-item">
@@ -103,21 +113,22 @@ function Skills() {
             <h3>Cursos</h3>
           </div>
           <div className="job-skill-summary">
-            <p>Node.js Essential Training - LinkedIn Learning - 2021</p>
-            <p>Scrum: The Basics - LinkedIn Learning - 2021</p>
-            <p>Learning REST APIs - LinkedIn Learning - 2021</p>
-            <p>React.js Essential Training - LinkedIn Learning - 2021</p>
-            <p>JavaScript Essential Training - LinkedIn Learning - 2021</p>
-            <p>Learning Django - LinkedIn Learning - 2021</p>
-            <p>CSS Essential Training – LinkedIn Learning - 2021</p>
-            <p>HTML Essential Training - LinkedIn Learning - 2021</p>
-            <p>MS-900: Microsoft 365 Fundamentals - Green Tecnologia - 2021</p>
-            <p>Learning Cloud Computing: Core Concepts - LinkedIn Learning - 2021</p>
-            <p>WordPress 5 Essential Training - LinkedIn Learning - 2021</p>
-            <p>JavaScript Essential Training (2017) - LinkedIn Learning - 2021</p>
-            <p>Python Automation and Testing - LinkedIn Learning - 2021</p>
-            <p>How to Make a Freaking Android App - Udemy - 2020</p>
-            <p>Automate the Boring Stuff with Python Programming - Udemy - 2020</p>  
+            <p>Node.js Essential Training - LinkedIn Learning - 2021.</p>
+            <p>Scrum: The Basics - LinkedIn Learning - 2021.</p>
+            <p>Learning REST APIs - LinkedIn Learning - 2021.</p>
+            <p>React.js Essential Training - LinkedIn Learning - 2021.</p>
+            <p>JavaScript Essential Training - LinkedIn Learning - 2021.</p>
+            <p>Learning Django - LinkedIn Learning - 2021.</p>
+            <p>CSS Essential Training – LinkedIn Learning - 2021.</p>
+            <p>HTML Essential Training - LinkedIn Learning - 2021.</p>
+            <p>SQL Essential Training - LinkedIn Learning - 2021.</p>
+            <p>MS-900: Microsoft 365 Fundamentals - Green Tecnologia - 2021.</p>
+            <p>Learning Cloud Computing: Core Concepts - LinkedIn Learning - 2021.</p>
+            <p>WordPress 5 Essential Training - LinkedIn Learning - 2021.</p>
+            <p>JavaScript Essential Training (2017) - LinkedIn Learning - 2021.</p>
+            <p>Python Automation and Testing - LinkedIn Learning - 2021.</p>
+            <p>How to Make a Freaking Android App - Udemy - 2020.</p>
+            <p>Automate the Boring Stuff with Python Programming - Udemy - 2020.</p>  
           </div>
         </section>
       </div>
